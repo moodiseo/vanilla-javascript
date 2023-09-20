0f4e2b7fc06831567a8b8e5ba0e034602fad3d15
slqkealr <135933231+slqkealr@users.noreply.github.com>
2023-09-20 16:31:21 +0900

    first commit

---
+++ momentum/js/greetings.js
@@ -0,0 +1,32 @@
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
+
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
+
+
+function onLogInFormSubmit(event){
+  event.preventDefault();
+  const username = loginInput.value;
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  localStorage.setItem(USERNAME_KEY,username);
+  paintGreeting(username);
+}
+
+function paintGreeting(username){
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+  greeting.innerText = `Hello! ${username}`;
+}
+
+loginForm.addEventListener("submit",onLogInFormSubmit);
+
+const savedUserName = localStorage.getItem(USERNAME_KEY);
+
+
+if(savedUserName === null){
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit",onLogInFormSubmit);
+}else{
+  paintGreeting(savedUserName);
+}
