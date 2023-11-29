607aba5eb21ef697875b31e83c2e22a3efbcf59f
moodiseo <moodiseo@gmail.com>
2023-11-29 11:32:52 +0900

    Recap

--- momentum/js/greetings.js
+++ momentum/js/greetings.js
@@ -8,22 +8,24 @@ const USERNAME_KEY = "username";
 function onLoginSubmit(event) {
   event.preventDefault();
   const username = loginInput.value;
-  loginForm.classList.add(HIDDEN_CLASSNAME);
-  localStorage.setItem(USERNAME_KEY, username);
-  paintGreeting(username);
+  localStorage.setItem("username", username);
+  loginForm.classList.add("hidden");
+  greeting.classList.remove("hidden");
+  greeting.innerText = `Hello! ${username}`;
 }
 
 loginForm.addEventListener("submit", onLoginSubmit);
 
 function paintGreeting(username) {
-  greeting.classList.remove(HIDDEN_CLASSNAME);
+  greeting.classList.remove("hidden");
   greeting.innerText = `Hello! ${username}`;
 }
 
-const savedUsername = localStorage.getItem(USERNAME_KEY);
+const savedUsername = localStorage.getItem("username");
+
 if (savedUsername === null) {
   loginForm.addEventListener("submit", onLoginSubmit);
-  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.classList.remove("hidden");
 } else {
   paintGreeting(savedUsername);
 }
