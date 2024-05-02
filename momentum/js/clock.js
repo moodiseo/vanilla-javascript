87424aa3363c03d7bde08d5cf663ca00d24e770a
moodiseo <moodiseo@gmail.com>
2024-05-02 16:19:55 +0900

    #5.2 PadStart

---
+++ momentum/js/clock.js
@@ -0,0 +1,12 @@
+const clock = document.querySelector("#clock");
+
+function getClock() {
+  const date = new Date();
+  const hours = date.getHours();
+  const minutes = date.getMinutes();
+  const seconds = date.getSeconds();
+  clock.innerText = `${hours}:${minutes}:${seconds}`;
+}
+
+getClock();
+setInterval(getClock, 1000);
