7a572df06fa4fb6125e22431e9f3d7b519248c9d
moodiseo <moodiseo@gmail.com>
2024-05-09 11:55:34 +0900

    #8.1 Weather API

--- momentum/js/weather.js
+++ momentum/js/weather.js
@@ -1,7 +1,17 @@
+const API_KEY = "1bc46b901fc8f99683d9355f99f36ce7";
+
 function onGeoOk(position) {
   const lat = position.coords.latitude;
-  const lng = position.coords.longitude;
-  console.log(lat, lng);
+  const lon = position.coords.longitude;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const weather = document.querySelector("#weather span:first-child");
+      const city = document.querySelector("#weather span:last-child");
+      weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+      city.innerText = data.name;
+    });
 }
 
 function onGeoError() {
