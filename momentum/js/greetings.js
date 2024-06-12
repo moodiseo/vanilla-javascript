965ace3456120f5896a9672720b83f59cb29c58e
moodiseo <moodiseo@gmail.com>
2024-06-12 11:29:20 +0900

    Recap-240612

--- momentum/js/greetings.js
+++ momentum/js/greetings.js
@@ -9,12 +9,12 @@ function onLoginSubmit(event) {
   event.preventDefault();
   const username = loginInput.value;
   localStorage.setItem(USERNAME_KEY, username);
-  loginForm.classList.add("hidden");
+  loginForm.classList.add(HIDDEN_CLASSNAME);
   paintGreeting(username);
 }
 
 function paintGreeting(username) {
-  greeting.classList.remove("hidden");
+  greeting.classList.remove(HIDDEN_CLASSNAME);
   greeting.innerText = `Hello ${username}`;
 }
 
@@ -22,7 +22,7 @@ const savedUsername = localStorage.getItem(USERNAME_KEY);
 
 if (savedUsername === null) {
   loginForm.addEventListener("submit", onLoginSubmit);
-  loginForm.classList.remove("hidden");
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
 } else {
   paintGreeting(savedUsername);
 }
